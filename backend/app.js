b648d046377de7fafc0effccb4735a04a3319393
Thomas-Ln <thomas-ln@laposte.net>
2021-09-16 19:13:53 +0200

    Add - express framework

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// allow Cross Origin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World !");
+});
+
+module.exports = app;
